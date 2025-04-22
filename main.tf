5feca4626afadfd1565645b05405230b0c322ebb
chandan K O <chandanjenn81@gmail.com>
2025-04-22 23:00:43 +0530

    changes

--- main.tf
+++ main.tf
@@ -68,19 +68,3 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
-#resource "azurerm_resource_group" "example" {
-#  name     = "example-resources"
-#  location = var.location
-#}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "votingstorage"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
